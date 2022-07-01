1262631c43d3140d917bfa5741996399cbc770a3
wyssttrrilla <90450924+wyssttrrilla@users.noreply.github.com>
2022-07-01 21:29:29 +0300

    finish

---
+++ AplusXamarinApp/AplusXamarinApp/ViewModel/ProjectListViewModel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+using System.ComponentModel;
+using AplusXamarinApp.Models;
+using AplusXamarinApp.Data;
+using AplusXamarinApp.Page;
+using AplusXamarinApp.Page.SecondSprint;
+
+namespace AplusXamarinApp.ViewModel
+{
+    public class ProjectListViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<ProjectViewModel> Projects { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public ICommand CreateCommand { protected set; get; }
+        public ICommand DeleteCommand { protected set; get; }
+        public ICommand SaveCommand { protected set; get; }
+        public ICommand BackCommand { protected set; get; }
+        ProjectViewModel selectedProject;
+        public INavigation Navigation { get; set; }
+        public ProjectListViewModel()
+        {
+            Projects = new ObservableCollection<ProjectViewModel>();
+            CreateCommand = new Command(CreateProject);
+            DeleteCommand = new Command(DeleteProject);
+            SaveCommand = new Command(SaveProject);
+            BackCommand = new Command(Back);
+        }
+
+        public ProjectViewModel SelectedFriend
+        {
+            get { return selectedProject; }
+            set
+            {
+                if (selectedProject != value)
+                {
+                    ProjectViewModel tempFriend = value;
+                    selectedProject = null;
+                    OnPropertyChanged("SelectedFriend");
+                    Navigation.PushAsync(new ProjectAdd());
+                }
+            }
+        }
+        protected void OnPropertyChanged(string propName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+
+        private void CreateProject()
+        {
+            Navigation.PushAsync(new ProjectAdd());
+        }
+        private void Back()
+        {
+            Navigation.PopAsync();
+        }
+        private void SaveProject(object friendObject)
+        {
+            ProjectViewModel friend = friendObject as ProjectViewModel;
+            if (friend != null && friend.IsValid && !Projects.Contains(friend))
+            {
+                Projects.Add(friend);
+            }
+            Back();
+        }
+        private void DeleteProject(object friendObject)
+        {
+            ProjectViewModel friend = friendObject as ProjectViewModel;
+            if (friend != null)
+            {
+                Projects.Remove(friend);
+            }
+            Back();
+        }
+    }
+}
---
+++ AplusXamarinApp/AplusXamarinApp/ViewModel/ProjectViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using AplusXamarinApp.Models;
+using AplusXamarinApp.ViewModel;
+
+namespace AplusXamarinApp.ViewModel
+{
+    public class ProjectViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        ProjectListViewModel lvm;
+        public Project Project { get; private set; }
+        public ProjectViewModel()
+        {
+            Project = new Project();
+        }
+        public ProjectListViewModel ListViewModel
+        {
+            get { return lvm; }
+            set
+            {
+                if (lvm != value)
+                {
+                    lvm = value;
+                    OnPropertyChanged("ListViewModel");
+                }
+            }
+        }
+        public string Name
+        {
+            get { return Project.ProjectName; }
+            set
+            {
+                if (Project.ProjectName != value)
+                {
+                    Project.ProjectName = value;
+                    OnPropertyChanged("Name");
+                }
+            }
+
+
+        }
+
+        public bool IsValid
+        {
+            get
+            {
+                return ((!string.IsNullOrEmpty(Name.Trim())));
+            }
+        }
+
+        protected void OnPropertyChanged(string propName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
